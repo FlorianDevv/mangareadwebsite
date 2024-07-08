cb89a1d9ac05ab443f9d03d5bdce8093014cf81f
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-07-08 16:24:33 +0200

    change logic for little value

--- components/magicui/number-ticker.tsx
+++ components/magicui/number-ticker.tsx
@@ -1,55 +1,58 @@
 "use client";
-
-import { useEffect, useRef } from "react";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 import { cn } from "@/lib/utils";
 
 export default function NumberTicker({
-  value,
-  direction = "up",
-  delay = 0,
-  className,
+	value,
+	direction = "up",
+	className,
+	delay = 0,
 }: {
-  value: number;
-  direction?: "up" | "down";
-  className?: string;
-  delay?: number; // delay in s
+	value: number;
+	direction?: "up" | "down";
+	className?: string;
+	delay?: number; // delay in s
 }) {
-  const ref = useRef<HTMLSpanElement>(null);
-  const motionValue = useMotionValue(direction === "down" ? value : 0);
-  const springValue = useSpring(motionValue, {
-    damping: 60,
-    stiffness: 100,
-  });
-  const isInView = useInView(ref, { once: true, margin: "0px" });
+	const ref = useRef<HTMLSpanElement>(null);
+	const motionValue = useMotionValue(direction === "down" ? value : 0);
 
-  useEffect(() => {
-    isInView &&
-      setTimeout(() => {
-        motionValue.set(direction === "down" ? 0 : value);
-      }, delay * 1000);
-  }, [motionValue, isInView, delay, value, direction]);
+	const config = {
+		damping: value < 10 ? 80 : 60,
+		stiffness: value < 10 ? 50 : 100,
+	};
 
-  useEffect(
-    () =>
-      springValue.on("change", (latest) => {
-        if (ref.current) {
-          ref.current.textContent = Intl.NumberFormat("en-US").format(
-            latest.toFixed(0),
-          );
-        }
-      }),
-    [springValue],
-  );
+	const springValue = useSpring(motionValue, config);
+	const isInView = useInView(ref, { once: true, margin: "0px" });
 
-  return (
-    <span
-      className={cn(
-        "inline-block tabular-nums text-black dark:text-white tracking-wider",
-        className,
-      )}
-      ref={ref}
-    />
-  );
+	useEffect(() => {
+		if (isInView) {
+			setTimeout(() => {
+				motionValue.set(direction === "down" ? 0 : value);
+			}, delay * 1000);
+		}
+	}, [motionValue, isInView, delay, value, direction]);
+
+	useEffect(() => {
+		const unsubscribe = springValue.on("change", (latest) => {
+			if (ref.current) {
+				ref.current.textContent = Intl.NumberFormat("en-US").format(
+					Math.round(latest),
+				);
+			}
+		});
+
+		return () => unsubscribe();
+	}, [springValue]);
+
+	return (
+		<span
+			className={cn(
+				"inline-block tabular-nums text-black dark:text-white tracking-wider",
+				className,
+			)}
+			ref={ref}
+		/>
+	);
 }
