8b0e6fd123602e3537251815e48e8fc68a8c0b82
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-07-08 15:31:50 +0200

    Add work in progress indicator to demo and documentation pages

--- app/demos/page.tsx
+++ app/demos/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
 	return (
 		<div>
 			<h1>Demos</h1>
+			<p className="text-2xl text-center mt-4">🚧 Working Progress 🚧</p>
 		</div>
 	);
 }
--- app/docs/installation/page.tsx
+++ app/docs/installation/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
 	return (
 		<div>
 			<h1>Getting started</h1>
+			<p className="text-2xl text-center mt-4">🚧 Working Progress 🚧</p>
 		</div>
 	);
 }
