5ee75e0a21e7102beca6840733895c82408eaed0
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-07-08 15:32:26 +0200

    chore: Add GitHub stars count to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,9 +4,11 @@ import {
 	NavigationMenuItem,
 	NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
-import { Github } from "lucide-react";
+import { Github, Star } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Étape 1
+import { useEffect, useState } from "react";
+import NumberTicker from "./magicui/number-ticker";
 import { ModeToggle } from "./modeToggle";
 
 export default function Navbar() {
@@ -16,6 +18,17 @@ export default function Navbar() {
 	const isActive = (path: string) => {
 		return pathname === path;
 	};
+
+	const [stars, setStars] = useState(null);
+
+	useEffect(() => {
+		fetch("https://api.github.com/repos/FlorianDevv/MangaRead")
+			.then((response) => response.json())
+			.then((data) => {
+				setStars(data.stargazers_count);
+			})
+			.catch((error) => console.error("Error stars fetch", error));
+	}, []);
 
 	return (
 		<nav className="fixed top-0 left-0 w-full z-10 flex items-center justify-between px-24 py-4 bg-white/25 dark:bg-black/25 backdrop-filter backdrop-blur-lg shadow-md">
@@ -47,6 +60,33 @@ export default function Navbar() {
 				</NavigationMenu>
 			</div>
 			<div className="flex items-center gap-4">
+				{/* github stars */}
+				{stars !== null && (
+					<>
+						<a
+							className="items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 hidden max-w-52 overflow-hidden whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2"
+							target="_blank"
+							href="https://github.com/FlorianDevv/MangaRead"
+							rel="noreferrer"
+						>
+							<span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
+							<div className="flex items-center">
+								<span className="ml-1">Star on GitHub</span>{" "}
+							</div>
+							<div className="ml-2 flex items-center gap-1 text-sm md:flex">
+								<Star
+									size={16}
+									className="transition-all duration-300 group-hover:text-yellow-300"
+								/>
+
+								<NumberTicker
+									value={stars}
+									className="text-white dark:text-black"
+								/>
+							</div>
+						</a>
+					</>
+				)}
 				<ModeToggle />
 				<a
 					href="https://github.com/FlorianDevv/Mangaread"
